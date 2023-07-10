8a1b77aeb247760f74bdf5805c20638bf9d68437
Juan Piza Pinzon <jpizapinzon@splunk.com>
2023-07-10 16:38:32 -0500

    First Commit

---
+++ src/test/java/com/restassured/test/TestExample.java
@@ -0,0 +1,11 @@
+package com.restassured.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestExample {
+    @Test
+    public void testSomething() {
+        Assert.assertEquals(500, 500);
+    }
+}
