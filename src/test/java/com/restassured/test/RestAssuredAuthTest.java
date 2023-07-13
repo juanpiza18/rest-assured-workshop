fd830900cc92317b51a569ee478eaaaa45107103
juanpiza18 <108146226+juanpiza18@users.noreply.github.com>
2023-07-13 13:48:56 -0500

    Hamcrest implementation add (#3)
    Co-authored-by: Juan Piza Pinzon <jpizapinzon@splunk.com>

--- src/test/java/com/restassured/test/RestAssuredAuthTest.java
+++ src/test/java/com/restassured/test/RestAssuredAuthTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.*;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestAssuredAuthTest extends BaseClassAuth{
 
@@ -14,6 +15,15 @@ public class RestAssuredAuthTest extends BaseClassAuth{
                 getStatusCode();
         System.out.println("Response code form server is " + statusCode);
         Assert.assertEquals(statusCode ,200);
+    }
+
+    @Test
+    public void test2WithHamcrestValidation() {
+        given()
+                .get()
+        .then()
+                .statusCode(200)
+                .body("authenticated", equalTo(true));
     }
 
 }
