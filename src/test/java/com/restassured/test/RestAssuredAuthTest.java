fd7444530139a3b4999eab4712e7fa9071a45791
Juan Piza Pinzon <jpizapinzon@splunk.com>
2023-07-18 13:16:10 -0500

    Add allure report generation and file upload example

--- src/test/java/com/restassured/test/RestAssuredAuthTest.java
+++ src/test/java/com/restassured/test/RestAssuredAuthTest.java
@@ -2,13 +2,18 @@ package com.restassured.test;
 
 import static io.restassured.RestAssured.*;
 
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.equalTo;
 
 public class RestAssuredAuthTest extends BaseClassAuth{
 
-    @Test
+    @Test(priority = 0, description="Valid Authentication Scenario with valid username and password.")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Test Description: Login test with valid username and password.")
+    @Story("Get authentication token")
+    @Step("Petition get to authentication")
     public void test1() {
         int statusCode = given().
                 get().
